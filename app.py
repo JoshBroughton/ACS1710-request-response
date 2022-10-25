a07971805de015a2f1b095dceddf1e39d71b6b63
Josh Broughton <josh.broughton47@gmail.com>
2022-10-25 10:57:14 -0600

    added favorite dessert route with route variable

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def favorite_animal(users_animal):
     """Display a message to the user that changes based on their favorite animal"""
     return f'Wow, {users_animal} is my favorite animal, too!'
 
+@app.route('/dessert/<users_desert>')
+def favorite_dessert(users_desert):
+    """Display a message to the user that changes based on their favorite dessert"""
+    return f'How did you know I liked {users_desert}?'
+
 if __name__ == '__main__':
     app.run(debug=True)
