360ef6e533db9982b530e2b40a3113388e638df6
Josh Broughton <josh.broughton47@gmail.com>
2022-10-25 11:02:26 -0600

    added madlib route and function with route variables

--- app.py
+++ app.py
@@ -17,5 +17,10 @@ def favorite_dessert(users_desert):
     """Display a message to the user that changes based on their favorite dessert"""
     return f'How did you know I liked {users_desert}?'
 
+@app.route('/madlibs/<adjective>/<noun>')
+def mad_libs(adjective, noun):
+    """Takes in two strings, an adjective and a noun, and displays a short story with them"""
+    return f'It is a little known fact that a {adjective} {noun} was responsible for the fall of Rome.'
+
 if __name__ == '__main__':
     app.run(debug=True)
